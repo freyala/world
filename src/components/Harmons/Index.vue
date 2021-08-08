ad08ad633783882f4bfb6779104f5c9f27a3249f
Tailchakra <chrisvdgeld@gmail.com>
2021-08-08 19:47:50 +0200

    Harmons push

---
+++ src/components/Harmons/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-wrap py-8">
+    <div class="w-full text-center mb-4">
+      <p class="text-3xl">
+        Sneak peak
+      </p>
+    </div>
+
+    <div class="flex w-full md:w-1/2 md:px-4">
+      <img class="m-auto" src="/harmons/harmon1.gif" alt="Harmon">
+    </div>
+    <div class="flex w-full md:w-1/2 md:px-4">
+      <img class="m-auto" src="/harmons/harmon2.gif" alt="Harmon">
+    </div>
+
+    <hr class="w-full mt-4">
+
+    <div class="w-full md:w-1/2 md:px-4">
+      <div class="small text-center">Drag me!</div>
+    </div>
+    <div class="w-full md:w-1/2 md:px-4">
+      <div class="small text-center">Drag me!</div>
+    </div>
+
+    <div class="w-full md:w-1/2 md:px-4">
+      <model-obj src="/harmons/harmon1.obj"></model-obj>
+    </div>
+    <div class="w-full md:w-1/2 md:px-4">
+      <model-obj src="/harmons/harmon2.obj"></model-obj>
+    </div>
+  </div>
+</template>
+<script>
+import { ModelObj } from 'vue-3d-model';
+export default {
+  components: { ModelObj }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import WorldMap from "../components/WorldMap/Index";
+import Harmons from "../components/Harmons/Index";
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/harmons',
+      name: 'harmons',
+      component: Harmons
+    },
     {
       path: '/',
       name: 'world-map',
