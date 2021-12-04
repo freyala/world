c49c719fb963c8df3667ea9823a90cd584eba7a7
nich <wei_hung.chin@daimler.com>
2021-12-04 10:37:30 +0800

    Fix css build warning

--- src/components/Marketplace/MarketPlaza.vue
+++ src/components/Marketplace/MarketPlaza.vue
@@ -2197,7 +2197,7 @@
     .market-plaza {
         display: flex;
         flex-wrap: wrap;
-        justify-content: start;
+        justify-content: flex-start;
     }
 
     .market-modal-enter-active,
