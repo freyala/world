ab8b666fd8b2c26420da480a4c2565d13697e7e5
Sandu Cezar <hopeslicer@gmail.com>
2021-12-09 00:57:21 +0200

    Piggie init

---
+++ src/components/Pigs/PiggyBar.vue
@@ -0,0 +1,136 @@
+<template>
+    <div v-on:click='$emit("click")' class="cursor-pointer mx-2 w-full flex h-full relative tmg-btn">
+        <div class='w-8/10 h-full flex flex-col justify-center items-center'>
+            <img class="inline" width="100%" v-bind:src="getButtonImage(attribute.name)" alt="Nap Button">
+            <p class='text-xs uppercase'>{{ attribute.eventName }}</p>
+        </div>
+        <div class='tmg-bar' v-bind:class='getFillBgColor(attribute)'>
+            <div v-bind:style='getBarFillMeter(attribute)' class='tmg-bar-fill' v-bind:class='getFillColor(attribute)'>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {
+        mapGetters,
+        mapActions
+    } from "vuex";
+
+    export default {
+        name: "PiggyBar",
+        props: {
+            attribute: {
+                type: Object,
+                default: undefined
+            }
+        },
+        data() {
+            return {};
+        },
+        mounted() {
+            console.log('ATTRIBUTE', this.attribute);
+        },
+        methods: {
+            ...mapGetters("exchange", ["getToken"]),
+            ...mapActions("exchange", ["resetTokens", "goTo"]),
+
+            getButtonImage(name) {
+                return `/pigs/${name}.svg`
+            },
+
+            getBarFillMeter(attribute) {
+                const fill = Math.min(1, attribute.current / attribute.max);
+                return {
+                    transform: `scaleY(${fill})`
+                };
+            },
+
+            getFillColor(attribute) {
+                const fillPercent = Math.min(1, attribute.current / attribute.max);
+                const color = fillPercent > 0.5 ? 'green' : fillPercent > 0.25 ? 'yellow' : 'red';
+
+                return `${color}-bar ${fillPercent < 0.25 ? "alert-fill" : ""}`;
+            },
+
+            getFillBgColor(attribute) {
+                const fillPercent = Math.min(1, attribute.current / attribute.max);
+                const color = fillPercent > 0.5 ? 'green' : fillPercent > 0.25 ? 'yellow' : 'red';
+
+                return `${color}-fill`;
+            }
+        },
+    };
+</script>
+
+<style scoped>
+    .tmg-btn {
+        min-width: 96px;
+        min-height: 96px;
+        padding: 8px;
+
+        background: white;
+        border: 1px solid #FBCCDE;
+        box-sizing: border-box;
+        border-radius: 12px;
+        margin: 0px 4px 0px px;
+    }
+
+    .green-bar {
+        background: linear-gradient(to bottom, #bbffe2 5%, #62C59A 5.5%, #62C59A 40.21%);
+
+    }
+
+    .yellow-bar {
+        background: linear-gradient(to bottom, #fff1ce 5%, #EDC357 6%, #EDC357 40.21%);
+    }
+
+    .red-bar {
+        background: linear-gradient(to bottom, #ffd2ce 5%, #ed6657 6%, #ed6657 40.21%);
+    }
+
+    .green-fill {
+        background: linear-gradient(to bottom, #CECECE 9.47%, #62C59A86 40.21%);
+    }
+
+
+    .yellow-fill {
+        background: linear-gradient(to bottom, #CECECE 9.47%, #EDC35786 40.21%);
+    }
+
+
+    .red-fill {
+        background: linear-gradient(to bottom, #CECECE 9.47%, #ed665786 40.21%);
+    }
+
+    .no-fill {
+        background-color: #CECECE;
+    }
+
+    .tmg-bar-fill {
+        width: 100%;
+        height: 100%;
+        transform-origin: 0 100%;
+        border-radius: 6px;
+        transition: all 0.5s ease-out;
+    }
+
+    .alert-fill {
+        animation: alert 0.5s infinite;
+    }
+
+    .tmg-bar {
+        width: 10px;
+        height: 100%;
+        margin: 0px 4px 0px 2px;
+        border-radius: 6px;
+        min-height: 90% !important;
+    }
+
+    @keyframes alert {
+        0% {
+            background-color: #ed6657 !important;
+            background: linear-gradient(to bottom, #CECECE00 0%, #CECECE00 100%);
+        }
+    }
+</style>
--- src/pages/CryptoPigs/Pigs.vue
+++ src/pages/CryptoPigs/Pigs.vue
@@ -1,102 +1,85 @@
 <template>
-  <section style=" min-height: 100vh" class="flex p-4 md:p-16 lg:px-32">
-    <div style="overflow-y: auto;" class="screen rounded-2xl w-full bg-pigs relative">
-      <div class="p-8 flex absolute top-0 w-full">
-        <div class="w-1/4 z-50">
-          <router-link :to="{path: 'marketplace', query: { market: '0xe5fd335819edb8da8395f8ec48beca747a0790ab' }}">
-            <img class="h-20 mb-4 mx-4" src="/pigs/Marketplace.svg" alt="Marketplace">
-          </router-link>
-        </div>
-        <div class="w-1/2 mx-auto text-center z-50">
-          <div class="relative flex">
-            <img class="h-20 inline m-auto" src="/pigs/NameBar.svg" alt="Name bar">
-            <input class="absolute pink top-0 left-0 pt-6 w-full text-center" type="text" v-model="titleText">
-          </div>
-          <div class="relative flex">
-            <img class="h-20 inline m-auto" src="/pigs/WelcomeButton.svg" alt="Welcome bar">
-            <input class="absolute top-0 left-0 text-xl text-white pt-6 text-xl w-full text-center" type="text" value="George">
-<!--            <input class="absolute top-0 left-0 text-xl text-white pt-5 text-xl w-full text-center" type="text" v-model="yourPigs[selectedPig].name">-->
-          </div>
-        </div>
-        <div class="w-1/4 ml-auto z-50">
-<!--          <img class="h-20 mb-4 mx-4" src="/pigs/GameSettings.svg" alt="GameSettings">-->
-<!--          <img class="h-20 mb-4 mx-4" src="/pigs/WalletSettings.svg" alt="WalletSettings">-->
-        </div>
+  <section style="min-height: 100vh; width: 40vw" class="flex p-4 mx-auto overflow-y-hidden md:p-16 lg:px-32">
+    <div class="screen rounded-2xl overflow-x-hidden w-full bg-dark relative">
+      <!-- MENU -->
+      <div v-if='showPiggyMenu' class='absolute top-0 bottom-0 right-0 left-0 bg-opacity-20 w-full h-full z-50 bg-dark'>
+
       </div>
+      <transition name='menu'>
+        <div v-if='showPiggyMenu' class='piggie-menu-container top-0 bottom-0 w-7/10 ' style=''>
 
-      <div class="flex flex-wrap p-4 md:p-8 relative h-full">
-        <div class="w-full flex mt-auto">
-          <img class="m-auto w-1/5 pt-60" src="/pigs/Pig.png" alt="Pig">
-<!--          <img class="m-auto w-1/4 pt-48" :src="yourPigs[selectedPig].image" alt="Pig">-->
+          <div class='w-full flex flex-col justify-center my-8'>
+            <h2 class='w-full text-center text-white text-2xl mb-4'>Your Piggies</h2>
+
+            <div class='w-full flex justify-center text-center items-center text-white text-lg opacity-50 cursor-pointer hover:opacity-100' v-for='piggy in yourPigs' :key='piggy.id'>
+                <img class='mt-1 mx-2' width="22px" src='/pigs/snout.svg' />
+                Pig #{{piggy.id}}
+            </div>
+          </div>
         </div>
 
-        <div class="w-full flex">
-          <div class="w-1/4 mt-auto">
-            <h6 class="pl-2 mb-2 text-2xl text-white">
-              Active pigs (3)
-            </h6>
-            <div class="pr-4 pink" style="height: 200px; overflow-y: auto">
-              <div class="flex flex-wrap">
-                <div class="w-full cursor-pointer bg-white rounded-xl px-4 py-2 mb-2" v-for="pig in yourPigs.slice(0,3)">
-                  {{ pig.name }} (age: 23)
+      </transition>
+      <!-- BACKGROUND -->
+      <div class="p-8 flex h-3/5 z-0 absolute top-0 w-full bg-pig-sky">
+      </div>
+      <div class='w-full h-3/5 z-0 absolute bottom-0 bg-pig-ground'>
+      </div>
+      <img class='absolute z-25' v-for='(cloud, index) in piggyClouds' :key='index'
+        v-bind:style='{"top": cloud.top + "px", "left": cloud.left + "px", opacity: cloud.opacity}'
+        v-bind:src='cloud.img' />
+      <img class='absolute z-10 right-2 w-7/10' style='top: 40%' src='/pigs/pigsty.png' />
+
+      <div class="flex flex-wrap p-4 md:p-8 z-10 relative h-full">
+
+        <!-- NAVBAR -->
+        <div class='absolute mx-auto h-20 z-50 flex w-9/10 top-4'>
+          <div class='cursor-pointer w-auto h-full piggie-menu-btn'>
+            <img class='h-full' src='/pigs/back_button.svg' />
+          </div>
+          <div v-on:click='showPiggyMenu = true' class='cursor-pointer ml-auto mr-4 w-auto h-full piggie-menu-btn'>
+            <img class='h-full' src='/pigs/piggies_button.svg' />
+          </div>
+          <div class='cursor-pointer w-auto h-full piggie-menu-btn'>
+            <img class='h-full' src='/pigs/wallet_button.svg' />
+          </div>
+          <div class='cursor-pointer w-auto mx-4 h-full piggie-menu-btn'>
+            <img class='h-full' src='/pigs/market_button.svg' />
+          </div>
+          <div class='cursor-pointer w-auto mr-4 h-full piggie-menu-btn'>
+            <img class='h-full' src='/pigs/settings_button.svg' />
+          </div>
+        </div>
+
+        <div class="w-full h-3/5 mt-14 relative flex items-center justify-center z-25">
+          <img v-for='(attribute, index) in selectedPig.attributes' :key='index' class="m-auto absolute w-7/10 pt-60"
+            v-bind:src='getPiggieAttributeImage(attribute)' alt="Pig">
+          <img class='m-auto absolute w-7/10 pt-60' v-bind:src='getPiggyWashStatus()' />
+        </div>
+
+        <div class='absolute mx-auto h-16 flex w-full left-0 bottom-40 z-50'>
+          <div class='mx-auto w-8/10 flex items-center justify-center rounded-2xl h-full bg-white'>
+            <h2 class='text-3xl w-5/10 text-center' style='color: #3C2F35'>George</h2>
+            <div class='w-5/10 relative h-20 bg-black rounded-2xl' style='background-color: #8660F1'>
+              <div class='flex w-full h-full justify-center'>
+                <div class='w-5/10 flex flex-col justify-center items-center cursor-pointer text-white text-4xl h-full'>
+                  <p class='h-3/5 pt-2 mb-1'>3</p>
+                  <p class='h-2/5 text-base'>Age</p>
+                </div>
+                <div class='w-5/10 flex flex-col justify-center items-center cursor-pointer text-white text-4xl h-full'>
+                  <p class='h-3/5 pt-2 mb-1 pt-4'>
+                    <img width='32px' src='/pigs/battle.svg' />
+                  </p>
+                  <p class='h-2/5 text-base'>Battle</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="w-1/3 flex mx-auto mt-auto pink">
-            <div class="cursor-pointer w-28 h-28 inline mx-auto relative">
-              <div class="z-20 absolute top-0 left-0 w-full h-full">
-                <img class="h-28 inline" src="/pigs/Eat Button.svg" alt="Eat Button">
-              </div>
-              <div class="z-10 absolute top-0 left-0 w-full h-full progress-container">
-                <div class="progress">
-                  <strong class="bar" :style="`height: ${eatBar}%`"></strong>
-                </div>
-              </div>
-            </div>
-            <div class="cursor-pointer w-28 h-28 inline mx-auto relative">
-              <div class="z-20 absolute top-0 left-0 w-full h-full">
-                <img class="h-28 inline" src="/pigs/Play Button.svg" alt="Play Button">
-              </div>
-              <div class="z-10 absolute top-0 left-0 w-full h-full progress-container">
-                <div class="progress">
-                  <strong class="bar" :style="`height: ${playBar}%`"></strong>
-                </div>
-              </div>
-            </div>
-            <div class="cursor-pointer w-28 h-28 inline mx-auto relative">
-              <div class="z-20 absolute top-0 left-0 w-full h-full">
-                <img class="h-28 inline" src="/pigs/Wash Button.svg" alt="Wash Button">
-              </div>
-              <div class="z-10 absolute top-0 left-0 w-full h-full progress-container">
-                <div class="progress">
-                  <strong class="bar" :style="`height: ${washBar}%`"></strong>
-                </div>
-              </div>
-            </div>
-            <div class="cursor-pointer w-28 h-28 inline mx-auto relative">
-              <div class="z-20 absolute top-0 left-0 w-full h-full">
-                <img class="h-28 inline" src="/pigs/Nap Button.svg" alt="Nap Button">
-              </div>
-              <div class="z-10 absolute top-0 left-0 w-full h-full progress-container">
-                <div class="progress">
-                  <strong class="bar" :style="`height: ${napBar}%`"></strong>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="w-1/4 mt-auto">
-            <h6 class="pl-2 mb-2 text-2xl text-white">
-              Inactive pigs ({{ yourPigs.length }})
-            </h6>
-            <div class="pr-4 pink" style="height: 200px; overflow-y: auto">
-              <div class="flex flex-wrap">
-                <div @click="activatePig(pig.id)" class="w-full cursor-pointer bg-white rounded-xl px-4 py-2 mb-2" v-for="pig in yourPigs.slice(3,999)">
-                  {{ pig.name }} (activate)
-                </div>
-              </div>
-            </div>
-          </div>
+        </div>
+
+        <!-- TAMAGOTCHI -->
+        <div :key='keys.piggyStats' class="w-9/10 flex h-20 mb-4 mt-auto pink">
+          <PiggyBar v-on:click='performAttributeAction(attribute)' v-for='(attribute, index) in piggyStats' :key='index'
+            :attribute='attribute'></PiggyBar>
         </div>
       </div>
     </div>
@@ -104,99 +87,318 @@
 </template>
 
 <script>
-import wallet from "../../plugins/wallet"
-import {ethers} from "ethers";
-import frey from "../../plugins/artifacts/frey.json";
-import axios from "axios";
-import {mapGetters} from "vuex";
+  import wallet from "../../plugins/wallet"
+  import {
+    ethers
+  } from "ethers";
+  import axios from "axios";
+  import {
+    mapGetters
+  } from "vuex";
 
-export default {
-  name: 'CryptoPigs',
-  mixins: [wallet],
-  computed: {
-    ...mapGetters([
-      'metaMaskAccount',
-      'metaMaskWallet'
-    ])
-  },
-  data() {
-    return {
-      eatBar: 15,
-      playBar: 45,
-      washBar: 67,
-      napBar: 5,
-      titleText: 'George be feeling tired ser',
-      pigAge: 0,
-      yourPigs: [],
-      selectedPig: 0,
-    }
-  },
-  mounted() {
-    this.getYourPigs()
-  },
-  methods: {
-    activatePig(id) {
-      console.log(id)
+  import Piggy from "../../plugins/artifacts/frey.json";
+  import AttributeManager from "../../plugins/artifacts/attributeManager.json";
+  import PiggyTamagotchi from "../../plugins/artifacts/tamagotchi.json";
+
+  import PiggyBar from '../../components/Pigs/PiggyBar.vue';
+
+  export default {
+    name: 'CryptoPigs',
+    mixins: [wallet],
+    components: {
+      PiggyBar
     },
-    async getYourPigs() {
-      const contract = new ethers.Contract('0xe5fd335819edb8da8395f8ec48beca747a0790ab', frey.abi, this.metaMaskWallet.signer)
-      const pigIds = await contract.tokensOfOwner(this.metaMaskAccount)
+    computed: {
+      ...mapGetters([
+        'metaMaskAccount',
+        'metaMaskWallet'
+      ]),
+    },
+    data() {
+      return {
+        attributeManagerContract: undefined,
+        tamagotchiContract: undefined,
 
-      let ids = await pigIds.map(async (frey) => {
-        return frey._isBigNumber ? ethers.BigNumber.from(frey._hex).toString() : frey._hex
-      })
+        titleText: 'George be feeling tired ser',
+        yourPigs: [],
+        keys: {
+          piggyStats: 0
+        },
+        selectedPig: 0,
 
-      await Promise.all(ids)
+        showPiggyMenu: false,
+
+        piggyNone: '/pigs/attributes/none.png',
+        piggyCloudsHandler: undefined,
+        piggyClouds: [],
+        piggyStats: [],
+        piggyActions: {
+          eating: false,
+          playing: false,
+          washing: false,
+          napping: false,
+        },
+      }
+    },
+    async mounted() {
+      this.getYourPigs();
+      this.initializeClouds();
+
+      this.attributeManagerContract = await new ethers.Contract(AttributeManager.address, AttributeManager.abi, this
+        .metaMaskWallet
+        .signer);
+      this.tamagotchiContract = await new ethers.Contract(PiggyTamagotchi.address, PiggyTamagotchi.abi, this
+        .metaMaskWallet.signer);
+
+
+      const attributes = await this.tamagotchiContract.getAttributeList();
+      const events = await this.tamagotchiContract.getPurchaseableEvents();
+
+      const freeEvents = events.filter(c => parseInt(c.price._hex) === 0);
+
+      for (let i = 0; i < attributes.length; i++) {
+        const event = freeEvents.filter(c => c.attributeName === attributes[i].attributeName)[0];
+        this.piggyStats.push({
+          name: attributes[i].attributeName,
+          threshold: attributes[i].thresholdValue * 1,
+          max: attributes[i].value * 1,
+          current: attributes[i].value * 1,
+          eventName: event.name
+        });
+      }
+    },
+    methods: {
+
+      initializeClouds() {
+        for (let i = 0; i < 4; i++) {
+          this.piggyClouds.push({
+            img: `/pigs/cloud_${1 + i % 3}.png`,
+            left: Math.random() * 156 - 200,
+            top: Math.random() * 200 - 100,
+            speed: Math.random() / 3 + 0.05,
+            opacity: 0.25 + Math.random()
+          })
+        }
+
+        const windowWidth = window.innerWidth * 0.4;
+        this.piggyCloudsHandler = setInterval(() => {
+          this.piggyClouds.forEach(cloud => {
+            cloud.left += cloud.speed;
+            if (cloud.stopped) return;
+            if (cloud.left > windowWidth * 1.1) {
+              cloud.stopped = true;
+              cloud.left = Math.random() * 156 - 400;
+              cloud.top = Math.random() * 200 - 100;
+              cloud.speed = Math.random() / 3 + 0.05;
+              setTimeout(() => {
+                cloud.stopped = false;
+              }, Math.random() * 2000);
+            }
+          });
+        }, 0);
+      },
+
+      activatePig(id) {},
+
+      getPiggyAttribute(attributeName) {
+        const attribute = this.piggyStats.filter(c => c.name === attributeName)[0];
+        return attribute;
+      },
+
+      async getYourPigs() {
+        const contract = new ethers.Contract('0xe5fd335819edb8da8395f8ec48beca747a0790ab', Piggy.abi, this
+          .metaMaskWallet.signer)
+        const pigIds = await contract.tokensOfOwner(this.metaMaskAccount)
+
+        let ids = await pigIds.map(async (Piggy) => {
+          return Piggy._isBigNumber ? ethers.BigNumber.from(Piggy._hex).toString() : Piggy._hex
+        })
+
+        await Promise.all(ids)
           .then(async (listOfIds) => {
 
             const yourPigs = await axios.get(`https://api.cryptopigs.one/meta/list?items=${listOfIds}`)
             this.yourPigs = yourPigs.data
             this.loading = false
 
+            if (this.yourPigs.length) {
+              this.selectedPig = this.yourPigs[0];
+            }
           })
+      },
+
+      getPiggieAttributeImage(attribute) {
+        if (attribute.trait_type === 'Background') return this.piggyNone;
+        const hygiene = this.piggyStats.filter(c => c.name === 'Hygiene')[0];
+        const energy = this.piggyStats.filter(c => c.name === 'Energy')[0];
+
+        if (!hygiene || !energy) return this.piggyNone;
+
+        if (attribute.trait_type === 'Eye') {
+          if (hygiene.current < 0.25 * hygiene.max) {
+            return '/pigs/attributes/Eye/Surprised.png';
+          }
+
+          if (energy.current < 0.25 * energy.max) {
+            return '/pigs/attributes/Eye/Sleepy.png';
+          }
+        }
+
+        if (attribute.trait_type === 'Front') {
+          if (this.piggyActions.eating || this.piggyActions.playing ||
+            hygiene.current <= hygiene.max * 0.25) {
+            return this.piggyNone;
+          }
+        }
+
+        return `/pigs/attributes/${attribute.trait_type}/${attribute.value}.png`;
+      },
+
+      getPiggyWashStatus(wash) {
+        const hygiene = this.piggyStats.filter(c => c.name === 'Hygiene')[0];
+        if (!hygiene) return this.piggyNone;
+
+        if (hygiene.current <= 0.25 * hygiene.max) {
+          return '/pigs/attributes/Modifiers/shitter.png';
+        } else if (hygiene.current < 0.5 * hygiene.max) {
+          return '/pigs/attributes/Modifiers/muddy.png';
+        }
+        return this.piggyNone;
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-div, input {
-  font-family: 'Chango', serif !important;
-}
+  div,
+  input {
+    font-family: 'Chango', serif !important;
+  }
 
-.pink {
-  color: #dc5689 !important;
-}
+  .bg-pig-sky {
+    background: url('/pigs/sky.png') no-repeat;
+    background-size: cover;
+  }
 
-.progress-container {
-  padding: 12px 15px 20px 15px;
-}
+  .bg-pig-ground {
+    background: url('/pigs/ground.png') no-repeat;
+    background-size: cover;
+  }
 
-.progress {
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  position: relative;
-}
+  .piggie-menu-btn:hover {
+    position: relative;
+    transform: scale(1.05);
+  }
 
-.bar {
-  width: 100%;
-  display: block;
-  font-size: 12px;
-  background-color: #bb9319;
-  color: #fff;
-  position: absolute;
-  bottom: 0;
-}
+  .piggie-jump {
+    animation: piggiejump 0.25s infinite ease-in-out;
+  }
 
-::-webkit-scrollbar-thumb {
-  background: #FFFFFF !important;
-  border-radius: 12px;
-}
+  @keyframes piggiejump {
+    0% {
+      transform: translateY(0px);
+    }
 
-input {
-  background: none;
-  outline: none;
-}
+    40% {
+      transform: translateY(-60px);
+    }
+
+    45% {
+      transform: translateY(-65px)
+    }
+
+    50% {
+      transform: translateY(-67px);
+    }
+
+    55% {
+      transform: translateY(-65px);
+    }
+
+    65% {
+      transform: translateY(-60px);
+    }
+
+    100% {
+      transform: translateY(0px);
+    }
+  }
+
+  .piggie-menu-container {
+    z-index: 5000;
+    position: absolute;
+    background: linear-gradient(269.03deg, #FF8AB6 0.85%, #F16097 99.18%);
+    box-shadow: 4px 0px 12px 6px rgba(0, 0, 0, 0.15);
+    border-radius: 16px;
+  }
+
+  .piggie-menu-btn:hover::after {
+    display: block;
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+
+  .piggie-btn {
+    min-width: 64px;
+    min-height: 64px;
+    background: #F16097;
+    border: 1px solid #FBCCDE;
+    box-sizing: border-box;
+    border-radius: 12px;
+  }
+
+  .pink {
+    color: #dc5689 !important;
+  }
+
+  .progress-container {
+    padding: 12px 15px 20px 15px;
+  }
+
+  .progress {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    position: relative;
+  }
+
+  .bar {
+    width: 100%;
+    display: block;
+    font-size: 12px;
+    background-color: #bb9319;
+    color: #fff;
+    position: absolute;
+    bottom: 0;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #FFFFFF !important;
+    border-radius: 12px;
+  }
+
+  input {
+    background: none;
+    outline: none;
+  }
+
+
+  .menu-enter-active,
+  .menu-leave-active {
+    transform: translateX(0);
+    opacity: 1;
+    transition: all .35s;
+  }
+
+  .menu-enter,
+  .menu-leave-to {
+    transforM: translateX(-200px);
+    opacity: 0.25;
+  }
 </style>
